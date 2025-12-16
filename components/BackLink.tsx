9e095a3e02b2b0a3bf51207a2082fba62608c8ff
reichstag <reichstagir@gmail.com>
2025-12-17 00:29:51 +0330

    fix back link bug

--- components/BackLink.tsx
+++ components/BackLink.tsx
@@ -11,12 +11,12 @@ import { ArrowRightIcon } from "lucide-react";
 interface IBackLink {
     href: Route;
     children: React.ReactNode;
-    classname?: string;
+    className?: string;
 }
 
-export function BackLink({ children, classname = "", href }: IBackLink) {
+export function BackLink({ children, className = "", href }: IBackLink) {
     return (
-        <Button asChild variant="ghost" className={cn("-ml-3", classname)}>
+        <Button asChild variant="ghost" className={cn("-ml-3", className)}>
             <Link
                 href={href}
                 className="flex gap-2 items-center text-sm text-muted-foreground"
